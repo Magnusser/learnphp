7c386e532d2591fb7908250a6acba6ec989a331f
Magnus Roland Raadla <magnus.roland.raadla@gmail.com>
2024-09-05 10:34:07 +0300

    sw

--- index.php
+++ index.php
@@ -1,18 +1,9 @@
 <?php
 
-$a = 'hello' . 'gamers';
 $array = [1,2,3];
+array_push($array,4,5,6);
+$array[] = 8;
+$array[4] = 123123;
 var_dump($array);
-
-$name = 'Magnus';
-
-$string = "hello $name\n";
-var_dump($string);
-$char = 'a';
-
-for($i=0;$i<26;$i++){
-    var_dump($char);
-    $char++;
-}
 
 ?>
