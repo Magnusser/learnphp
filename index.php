ea45e8213265988f89b54db5e207ada2ae0b2390
Magnus Roland Raadla <magnus.roland.raadla@gmail.com>
2024-09-05 10:19:00 +0300

    ig

--- index.php
+++ index.php
@@ -1,5 +1,18 @@
 <?php
 
-echo 'hello world';
+$a = 'hello' . 'gamers';
+$array = [1,2,3];
+var_dump($array);
+
+$name = 'Magnus';
+
+$string = "hello $name\n";
+var_dump($string);
+$char = 'a';
+
+for($i=0;$i<26;$i++){
+    var_dump($char);
+    $char++;
+}
 
 ?>
