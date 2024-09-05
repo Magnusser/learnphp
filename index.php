d3398693310bc6299e0fd225f238001b2d4c5def
Magnus Roland Raadla <magnus.roland.raadla@gmail.com>
2024-09-05 11:22:44 +0300

    llpllöö

--- index.php
+++ index.php
@@ -10,12 +10,24 @@ class Box {
     }
 }
 
+class MetalBox extends Box {
+    public $weightPerUnit = 10;
+    public function weight(){
+        return $this->volume() * $this->weightPerUnit;
+    }
+}
+
+$num1 = 1;
+$num2 = $num1;
+$num2 = 2;
+var_dump($num1, $num2);
+
 $box1 = new Box();
 $box1->height = 1;
 $box1->width = 1;
 $box1->length = 1;
 var_dump($box1);
-$box2 = new Box();
+$box2 = new MetalBox();
 $box2->height = 2;
 $box2->width = 100;
 $box2->length = 0.3;
