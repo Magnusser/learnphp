364077e75143e9429c374b2acf245449a9630f94
Magnus Roland Raadla <magnus.roland.raadla@gmail.com>
2024-09-06 13:22:53 +0300

    ddwdd

--- index.php
+++ index.php
@@ -1,47 +1,39 @@
 <?php
 
-class Box {
-    public $height;
-    public $width;
-    public $length;
-
-    public function volume(){
-        return $this->height * $this->width * $this->length;
+class ConsoleLogger implements Logger{
+    public function log($message){
+        echo "$message\n";
     }
 }
 
-class MetalBox extends Box {
-    use HasMatrerial;
-    public $weightPerUnit = 10;
-    public function weight(){
-        return $this->volume() * $this->weightPerUnit;
+class Task {
+    public function work($logger){
+        for($i=0;$i<10;$i++){
+            $logger->log($i);
+        }
     }
 }
 
-trait HasMatrerial {
-    public $material;
-    public function getMaterial() {
-        return $this->material;
+class NothingLogger implements Logger{
+    public function log($message){
+
     }
 }
 
-$num1 = 1;
-$num2 = $num1;
-$num2 = 2;
-var_dump($num1, $num2);
+class FileLogger implements Logger{
+    public function log($message){
+        $file = fopen('log.txt', 'a');
+        fwrite($file, "$message\n");
+        fclose($file);
+    }
+}
 
-$box1 = new Box();
-$box1->height = 1;
-$box1->width = 1;
-$box1->length = 1;
-var_dump($box1);
-$box2 = new MetalBox();
-$box2->height = 2;
-$box2->width = 100;
-$box2->length = 0.3;
-var_dump($box1);
-var_dump($box2);
+interface Logger {
+    public function 
+}
 
-var_dump($box1->volume());
-var_dump($box2->volume());
+$logger = new FileLogger();
+$task = new Task();
+$task->work();
+
 ?>
