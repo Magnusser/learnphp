e30c1d817bd17cd54364d997362a46b73cf21b57
Magnus Roland Raadla <magnus.roland.raadla@gmail.com>
2024-09-12 11:25:53 +0300

    huhhh

--- public/index.php
+++ public/index.php
@@ -16,50 +16,10 @@ spl_autoload_register(function($class){
     require_once "src/$class.php";
 });
 
-// require_once 'src/Router.php';
-// require_once 'src/DB.php';
+require 'routes.php';
 
-use App\Controllers\PublicController as PC;
-
-$router = new App\Router();
-$db = new App\DB();
-$controller = new PC();
-$controller = new PC();
-$controller = new PC();
-$controller = new PC();
-$controller = new PC();
-$controller = new PC();
-$controller = new PC();
-$controller = new PC();
-dump($router, $db, $controller);
-// switch($_SERVER['REQUEST_URI']){
-//     case '/':
-//         $posts = [
-//             ['title' => 'Some title 1', 'body' => 'Some body 1'],
-//             ['title' => 'Some title 2', 'body' => 'Some body 2'],
-//             ['title' => 'Some title 3', 'body' => 'Some body 3'],
-//             ['title' => 'Some title 4', 'body' => 'Some body 4'],
-//         ];
-//         include 'views/index.php';
-//         break;
-//     case '/us':
-//         $posts = [
-//             ['title' => 'Some us title 1', 'body' => 'Some us body 1'],
-//             ['title' => 'Some us title 2', 'body' => 'Some us body 2'],
-//             ['title' => 'Some us title 3', 'body' => 'Some us body 3'],
-//             ['title' => 'Some us title 4', 'body' => 'Some us body 4'],
-//         ];
-//         include 'views/us.php';
-//         break;
-//     case '/tech':
-//         $posts = [
-//             ['title' => 'Some tech title 1', 'body' => 'Some tech body 1'],
-//             ['title' => 'Some tech title 2', 'body' => 'Some tech body 2'],
-//             ['title' => 'Some tech title 3', 'body' => 'Some tech body 3'],
-//             ['title' => 'Some tech title 4', 'body' => 'Some tech body 4'],
-//         ];
-//         include 'views/tech.php';
-//         break;
-//     default:
-//         echo 'ERROR 404!';
-// }
+$router = new App\Router($_SERVER['REQUEST_URI']);
+$match = $router->match();
+if($match){
+    call_user_func($match['action']);
+}
