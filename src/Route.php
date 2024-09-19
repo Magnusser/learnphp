e998e94f3676046d64dee9e424820441bc6fe31e
Magnus Roland Raadla <magnus.roland.raadla@gmail.com>
2024-09-19 08:22:45 +0300

    g

--- src/Route.php
+++ src/Route.php
@@ -7,17 +7,17 @@ class Route {
     public $action;
     public $method;
 
-    public function __construct($method, $path, $action){
+    public function __construct($method, $path, $action)
+    {
         $this->method = $method;
         $this->path = $path;
         $this->action = $action;
     }
 
     public static function get($path, $action){
-        Route::addRoute('GET' $path, $action);
+        Router::addRoute('GET', $path, $action);
     }
-
     public static function post($path, $action){
-        Route::addRoute('POST' $path, $action);
+        Router::addRoute('POST', $path, $action);
     }
 }
