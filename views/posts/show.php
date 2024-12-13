3035f81be6d57ac5b260c6e61245e17b01036214
Marsiepie <115626319+Magnusser@users.noreply.github.com>
2024-12-13 17:46:45 +0200

    Update show.php

--- views/posts/show.php
+++ views/posts/show.php
@@ -1,15 +1,4 @@
 <?php include __DIR__ . '/../partials/header.php'; ?>
-
-<!-- <style>
-table, th, td {
-  border:2px solid grey;
-  background-color: lightgrey;
-}
-
-
-
-</style> -->
-
 <main class="container">
     <table class="table table-hover table-striped">
       <tr>
@@ -19,14 +8,10 @@ table, th, td {
       </tr>
       <tr>
       <th>Content</th>
-      <!-- tühikuid ei tohi panna < ?=$post->title? >e need muidu liituvad teksti algusesse--></td>
+      </td>
         <td><?=$post->body?></td>
       </tr>
     </table>
-    <!-- method default on GET -->
-    <!-- kui actionit pole pandud siis ta tuleb samale leheküljele tagasi -->
-
-
 </main>
 
 <?php include __DIR__ . '/../partials/footer.php'; ?>
